61f48881a8b0fd7669081e1af8060bd1c3023f91
Jack McDonald <124325301+jackmcdonald-byte@users.noreply.github.com>
2025-03-30 15:41:12 -0400

    Cleanup

--- src/Python/main/chassis.py
+++ src/Python/main/chassis.py
@@ -26,7 +26,7 @@ class Chassis:
     :type MotorController: MotorController
     Authors: Jack McDonald, Ralph Calabrese
     """
-    def __init__(self):
+        def __init__(self, robot: Robot):
         """
         Represents the main class responsible for initializing the 
         MotorController object. This class serves as the entry point 
@@ -39,55 +39,61 @@ class Chassis:
         Author: Jack McDonald
         """
         self.MotorController = MotorController()
+        self.robot = robot
 
     def move_until_colour(self, colour: str):
-    """
-    Moves the robot until the specified colour is detected. The movement stops when the given
-    colour is identified. This function assumes a mechanism to detect colours and halts operation
-    when the desired condition is fulfilled.
+        """
+        Moves the robot until the specified colour is detected. The movement stops when the given
+        colour is identified. This function assumes a mechanism to detect colours and halts operation
+        when the desired condition is fulfilled.
 
-    :param colour: The target colour to be detected during the movement.
-    :type colour: str
-    :return: None
-    """
-    self.MotorController.move_forward()
-    while self.robot.get_colour() != colour:
-        pass
-    self.MotorController.stop()
+        :param colour: The target colour to be detected during the movement.
+        :type colour: str
+        :return: None
+        """
+        self.MotorController.move_forward()
+        while self.robot.get_colour() != colour:
+            pass
+        self.MotorController.stop()
 
     def move_until_distance(self, distance: int):
         """
-        Move the robot until it covers the specified distance in centimeters.
-        
-        This method allows the robot to move forward until the specified
-        distance is reached. The distance is measured in centimeters and
-        converted internally to the units used by the robot's navigation system.
-        Ensure that appropriate conditions and validations are set for safe operation.
-        
-        :param distance: The distance in centimeters the robot should cover 
-                        before stopping.
+        Move the robot until the distance to an object is less than or equal to
+        the specified value in centimeters.
+
+        This method allows the robot to move forward until the distance
+        to an object falls below or equals the specified threshold. The
+        distance is measured in centimeters using the robot's sensors.
+
+        :param distance: The maximum distance in centimeters to an object
+                        before the robot should stop moving.
         :type distance: int
         :return: None
+        Author: Jack McDonald
         """
-        
+        self.MotorController.move_forward()
+        while self.robot.get_distance() > distance:
+            pass
+        self.MotorController.stop()
 
     def move_one_tile(self):
-    """
-    Moves the robot one tile further by detecting and crossing a black line.
-    
-    :return: None
-    :raises TimeoutError: If line isn't detected within reasonable time
-    """        
-   try:
-        # Move forward until black line is detected
-        self.move_until_colour("black")
-        
-        # Move past the line by specified overrun distance
-        self.move_until_distance(OVERRUN_DISTANCE)  
+        """
+        Moves the robot one tile further in the current direction of movement by 
+        using the colour sensor to verify that has crossed a black line. 
+        This function assumes the robot's movement direction and its environment 
+        are predefined and does not take any argument.
 
-    except Exception as e:
-        self.MotorController.stop()
-        raise
+        :return: None
+        """      
+        try:
+            # Move forward until black line is detected
+            self.move_until_colour("black")
+        
+            # Move past the line by specified overrun distance
+            self.move_until_distance(OVERRUN_DISTANCE)  
+
+         except Exception as e:
+            self.MotorController.stop()
             
     def turn_right(self):
         """
